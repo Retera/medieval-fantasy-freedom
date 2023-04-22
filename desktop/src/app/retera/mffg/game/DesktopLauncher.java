a340549d9a390b958dc45421a5fbefd45278444b
Retera <retera@etheller.com>
2023-04-22 15:13:41 -0400

    Initial commit

---
+++ desktop/src/app/retera/mffg/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package app.retera.mffg.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import app.retera.mffg.game.MedFanFreeGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Medieval Fantasy Freedom Game");
+		new Lwjgl3Application(new MedFanFreeGame(), config);
+	}
+}
